3fc9281a8a98838ffd1dce98d19f4c1bf7b9d37d
Anthony Young <github@ayoungh.co.uk>
2016-10-28 11:40:40 +0100

    Mobx learning
    https://www.youtube.com/watch?v=_q50BXqkAfI

---
+++ MobX Todo/src/js/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoList from './TodoList';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<TodoList/>, app);
